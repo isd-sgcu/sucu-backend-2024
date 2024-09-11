401005ab750475533bf609cba993c353e24d8ba7
Gear <84141000+macgeargear@users.noreply.github.com>
2024-09-11 19:26:10 +0700

    fix: add required tag in `CreateUserDTO`

--- internal/interface/dtos/user_dto.go
+++ internal/interface/dtos/user_dto.go
@@ -12,11 +12,11 @@ type UserDTO struct {
 }
 
 type CreateUserDTO struct {
-	ID        string `json:"id"`         // student id
-	FirstName string `json:"first_name"` // user's first name
-	LastName  string `json:"last_name"`  // user's last name
-	Password  string `json:"password"`   // user's password
-	Role      string `json:"role"`       // role: sgcu-admin, sgcu-superadmin, sccu-admin, sccu-superadmin
+	ID        string `json:"id" validate:"required"`         // student id
+	FirstName string `json:"first_name" validate:"required"` // user's first name
+	LastName  string `json:"last_name" validate:"required"`  // user's last name
+	Password  string `json:"password" validate:"required"`   // user's password
+	Role      string `json:"role" validate:"required"`       // role: sgcu-admin, sgcu-superadmin, sccu-admin, sccu-superadmin
 }
 
 type UpdateUserDTO struct {
