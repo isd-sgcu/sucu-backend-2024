9729b33230cd6a94ef9761808f1de8b5c4620ed8
Gear <84141000+macgeargear@users.noreply.github.com>
2024-09-12 23:47:18 +0700

    fix: remove required field from role

--- internal/interface/dtos/user_dto.go
+++ internal/interface/dtos/user_dto.go
@@ -16,7 +16,7 @@ type CreateUserDTO struct {
 	FirstName string `json:"first_name" validate:"required"` // user's first name
 	LastName  string `json:"last_name" validate:"required"`  // user's last name
 	Password  string `json:"password" validate:"required"`   // user's password
-	Role      string `json:"role" validate:"required"`       // role: sgcu-admin, sgcu-superadmin, sccu-admin, sccu-superadmin
+	Role      string `json:"role"`                           // role: sgcu-admin, sgcu-superadmin, sccu-admin, sccu-superadmin
 }
 
 type UpdateUserDTO struct {
