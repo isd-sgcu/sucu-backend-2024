8892dd067af72edc0e504244f9b3db0995ae11ef
Idhibhat Pankam <marginpankam@gmail.com>
2024-09-13 23:32:30 +0700

    apperror folder

--- internal/domain/usecases/user.go
+++ internal/domain/usecases/user.go
@@ -2,17 +2,17 @@ package usecases
 
 import (
 	"github.com/isd-sgcu/sucu-backend-2024/internal/interface/dtos"
-	"github.com/isd-sgcu/sucu-backend-2024/pkg/response"
+	"github.com/isd-sgcu/sucu-backend-2024/pkg/apperror"
 )
 
 type UserUsecase interface {
 	// super-admin method
-	GetAllUsers(req *dtos.UserDTO) (*[]dtos.UserDTO, *response.AppError)
-	GetUserByID(req *dtos.UserDTO, userID string) (*dtos.UserDTO, *response.AppError)
-	CreateUser(req *dtos.UserDTO, createUserDTO *dtos.CreateUserDTO) *response.AppError
-	UpdateUserByID(req *dtos.UserDTO, userID string, updateUserDTO *dtos.UpdateUserDTO) *response.AppError
-	DeleteUserByID(req *dtos.UserDTO, userID string) *response.AppError
+	GetAllUsers(req *dtos.UserDTO) (*[]dtos.UserDTO, *apperror.AppError)
+	GetUserByID(req *dtos.UserDTO, userID string) (*dtos.UserDTO, *apperror.AppError)
+	CreateUser(req *dtos.UserDTO, createUserDTO *dtos.CreateUserDTO) *apperror.AppError
+	UpdateUserByID(req *dtos.UserDTO, userID string, updateUserDTO *dtos.UpdateUserDTO) *apperror.AppError
+	DeleteUserByID(req *dtos.UserDTO, userID string) *apperror.AppError
 
 	// admin method
-	UpdateProfile(req *dtos.UserDTO, updateUserDTO *dtos.UpdateUserDTO) *response.AppError
+	UpdateProfile(req *dtos.UserDTO, updateUserDTO *dtos.UpdateUserDTO) *apperror.AppError
 }
--- internal/domain/usecases/user_usecase.go
+++ internal/domain/usecases/user_usecase.go
@@ -6,8 +6,8 @@ import (
 	"github.com/isd-sgcu/sucu-backend-2024/internal/domain/entities"
 	"github.com/isd-sgcu/sucu-backend-2024/internal/interface/dtos"
 	"github.com/isd-sgcu/sucu-backend-2024/internal/interface/repositories"
+	"github.com/isd-sgcu/sucu-backend-2024/pkg/apperror"
 	"github.com/isd-sgcu/sucu-backend-2024/pkg/config"
-	"github.com/isd-sgcu/sucu-backend-2024/pkg/response"
 	"github.com/isd-sgcu/sucu-backend-2024/utils"
 	"github.com/isd-sgcu/sucu-backend-2024/utils/constant"
 	"go.uber.org/zap"
@@ -30,36 +30,36 @@ func NewUserUsecase(cfg config.Config, logger *zap.Logger, userRepository reposi
 
 // super-admin method
 
-func (u *userUsecase) GetAllUsers(req *dtos.UserDTO) (*[]dtos.UserDTO, *response.AppError) {
+func (u *userUsecase) GetAllUsers(req *dtos.UserDTO) (*[]dtos.UserDTO, *apperror.AppError) {
 	return nil, nil
 }
 
-func (u *userUsecase) GetUserByID(req *dtos.UserDTO, userID string) (*dtos.UserDTO, *response.AppError) {
+func (u *userUsecase) GetUserByID(req *dtos.UserDTO, userID string) (*dtos.UserDTO, *apperror.AppError) {
 	return nil, nil
 }
 
-func (u *userUsecase) CreateUser(req *dtos.UserDTO, createUserDTO *dtos.CreateUserDTO) *response.AppError {
+func (u *userUsecase) CreateUser(req *dtos.UserDTO, createUserDTO *dtos.CreateUserDTO) *apperror.AppError {
 	role, err := utils.GetRole(createUserDTO.Role)
 	if err != nil {
 		u.logger.Named("CreateUser").Error(constant.ErrInvalidRole, zap.String("role", createUserDTO.Role), zap.Error(err))
-		return response.BadRequestError(constant.ErrInvalidRole)
+		return apperror.BadRequestError(constant.ErrInvalidRole)
 	}
 
 	existingUser, err := u.userRepository.FindUserByID(createUserDTO.ID)
 	if err != nil && !errors.Is(err, gorm.ErrRecordNotFound) {
 		u.logger.Named("CreateUser").Error(constant.ErrFindUserByID, zap.String("userID", createUserDTO.ID), zap.Error(err))
-		return response.NotFoundError(constant.ErrFindUserByID)
+		return apperror.NotFoundError(constant.ErrFindUserByID)
 	}
 
 	if existingUser != nil {
 		u.logger.Named("CreateUser").Error(constant.ErrUserAlreadyExists, zap.String("userID", createUserDTO.ID))
-		return response.BadRequestError(constant.ErrUserAlreadyExists)
+		return apperror.BadRequestError(constant.ErrUserAlreadyExists)
 	}
 
 	hashedPassword, err := utils.HashPassword(createUserDTO.Password)
 	if err != nil {
 		u.logger.Named("CreateUser").Error(constant.ErrHashPasswordFailed, zap.String("userID", createUserDTO.ID), zap.Error(err))
-		return response.InternalServerError(constant.ErrHashPasswordFailed)
+		return apperror.InternalServerError(constant.ErrHashPasswordFailed)
 	}
 
 	newUser := &entities.User{
@@ -72,23 +72,23 @@ func (u *userUsecase) CreateUser(req *dtos.UserDTO, createUserDTO *dtos.CreateUs
 
 	if err := u.userRepository.InsertUser(newUser); err != nil {
 		u.logger.Named("CreateUser").Error(constant.ErrInsertUserFailed, zap.String("userID", req.ID), zap.Error(err))
-		return response.InternalServerError(constant.ErrInsertUserFailed)
+		return apperror.InternalServerError(constant.ErrInsertUserFailed)
 	}
 
 	u.logger.Named("CreateUser").Info("Success: ", zap.String("user_id", newUser.ID))
 	return nil
 }
 
-func (u *userUsecase) UpdateUserByID(req *dtos.UserDTO, userID string, updateUserDTO *dtos.UpdateUserDTO) *response.AppError {
+func (u *userUsecase) UpdateUserByID(req *dtos.UserDTO, userID string, updateUserDTO *dtos.UpdateUserDTO) *apperror.AppError {
 	return nil
 }
 
-func (u *userUsecase) DeleteUserByID(req *dtos.UserDTO, userID string) *response.AppError {
+func (u *userUsecase) DeleteUserByID(req *dtos.UserDTO, userID string) *apperror.AppError {
 	return nil
 }
 
 // admin method
 
-func (u *userUsecase) UpdateProfile(req *dtos.UserDTO, updateUserDTO *dtos.UpdateUserDTO) *response.AppError {
+func (u *userUsecase) UpdateProfile(req *dtos.UserDTO, updateUserDTO *dtos.UpdateUserDTO) *apperror.AppError {
 	return nil
 }
