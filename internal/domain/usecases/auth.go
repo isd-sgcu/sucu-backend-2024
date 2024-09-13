a19ebf0716392e1a4782bdb400c866263743956b
Idhibhat Pankam <marginpankam@gmail.com>
2024-09-13 23:35:21 +0700

    auth

--- internal/domain/usecases/auth.go
+++ internal/domain/usecases/auth.go
@@ -1,7 +1,10 @@
 package usecases
 
-import "github.com/isd-sgcu/sucu-backend-2024/internal/interface/dtos"
+import (
+	"github.com/isd-sgcu/sucu-backend-2024/internal/interface/dtos"
+	"github.com/isd-sgcu/sucu-backend-2024/pkg/apperror"
+)
 
 type AuthUsecase interface {
-	Login(loginUserDTO *dtos.LoginUserDTO) (*dtos.LoginResponseDTO, error)
+	Login(loginUserDTO *dtos.LoginUserDTO) (*dtos.LoginResponseDTO, *apperror.AppError)
 }
