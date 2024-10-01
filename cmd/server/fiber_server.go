78c8b737ebd821d00d8cfce371ab66eb7c1335c3
Dalai Jotisalikorn Surenjargal <dalai2547@gmail.com>
2024-10-02 02:38:48 +0700

    fix: change to patch

--- cmd/server/fiber_server.go
+++ cmd/server/fiber_server.go
@@ -134,7 +134,7 @@ func (s *FiberHttpServer) initDocumentRouter(router fiber.Router, httpHandler ha
 	documentRouter := router.Group("/documents")
 
 	documentRouter.Post("/", httpHandler.Middleware().IsLogin, httpHandler.Document().CreateDocument)
-	documentRouter.Put("/:document_id", httpHandler.Middleware().IsLogin, httpHandler.Middleware().SuperAdmin, httpHandler.Document().UpdateDocumentByID)
+	documentRouter.Patch("/:document_id", httpHandler.Middleware().IsLogin, httpHandler.Middleware().SuperAdmin, httpHandler.Document().UpdateDocumentByID)
 	documentRouter.Delete("/:document_id", httpHandler.Middleware().IsLogin, httpHandler.Middleware().SuperAdmin, httpHandler.Document().DeleteDocumentByID)
 
 }
