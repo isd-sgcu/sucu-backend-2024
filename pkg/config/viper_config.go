3622949c268221c6145ffe39f5319340943b7dec
wiraphat <132466647+wiraphatys@users.noreply.github.com>
2024-11-17 20:39:32 +0700

    chore: update env config setup in prod

--- pkg/config/viper_config.go
+++ pkg/config/viper_config.go
@@ -2,6 +2,8 @@ package config
 
 import (
 	"log"
+	"os"
+	"strconv"
 	"sync"
 
 	"github.com/spf13/viper"
@@ -21,25 +23,89 @@ var (
 
 func NewViperConfig() Config {
 	once.Do(func() {
-		v := viper.New()
-		// v.SetConfigFile(".env")
-		v.AutomaticEnv()
-
-		if err := v.ReadInConfig(); err != nil {
-			log.Fatalf("Error reading configs file: %s", err)
+		serverEnv := os.Getenv("SERVER_ENV")
+		if serverEnv == "production" {
+			loadProductionEnv()
+		} else {
+			loadDefaultEnv()
 		}
-
-		cfg := &viperConfig{}
-
-		err := v.Unmarshal(cfg)
-		if err != nil {
-			log.Fatalf("Unable to decode into struct, %v", err)
-		}
-
-		instance = cfg
 	})
-
 	return instance
+}
+
+func loadProductionEnv() {
+	log.Println("Loading production environment variables")
+	instance = &viperConfig{
+		Server: Server{
+			Name: os.Getenv("SERVER_NAME"),
+			Env:  os.Getenv("SERVER_ENV"),
+			Url:  os.Getenv("SERVER_URL"),
+			Host: os.Getenv("SERVER_HOST"),
+			Port: func() int {
+				port, err := strconv.Atoi(os.Getenv("SERVER_PORT"))
+				if err != nil {
+					panic("error while loading server port")
+				}
+				return port
+			}(),
+		},
+		Db: Db{
+			Host: os.Getenv("DB_HOST"),
+			Port: func() int {
+				port, err := strconv.Atoi(os.Getenv("DB_PORT"))
+				if err != nil {
+					panic("error while loading db port")
+				}
+				return port
+			}(),
+			User:     os.Getenv("DB_USER"),
+			Password: os.Getenv("DB_PASS"),
+			Name:     os.Getenv("DB_NAME"),
+			SSLMode:  os.Getenv("DB_SSL_MODE"),
+			Timezone: os.Getenv("DB_TIMEZONE"),
+		},
+		Jwt: Jwt{
+			ApiSecretKey:       os.Getenv("JWT_API_SECRET_KEY"),
+			AccessTokenSecret:  os.Getenv("JWT_ACCESS_TOKEN_SECRET"),
+			RefreshTokenSecret: os.Getenv("JWT_REFRESH_TOKEN_SECRET"),
+			AccessTokenExpiration: func() int {
+				expiration, err := strconv.Atoi(os.Getenv("JWT_ACCESS_TOKEN_EXPIRATION"))
+				if err != nil {
+					panic("error while loading access token expiration")
+				}
+				return expiration
+			}(),
+			RefreshTokenExpiration: func() int {
+				expiration, err := strconv.Atoi(os.Getenv("JWT_REFRESH_TOKEN_EXPIRATION"))
+				if err != nil {
+					panic("error while loading refresh token expiration")
+				}
+				return expiration
+			}(),
+		},
+		Aws: Aws{
+			BucketName:      os.Getenv("AWS_BUCKET_NAME"),
+			AccessKeyId:     os.Getenv("AWS_ACCESS_KEY_ID"),
+			SecretAccessKey: os.Getenv("AWS_SECRET_ACCESS_KEY"),
+			Region:          os.Getenv("AWS_REGION"),
+		},
+	}
+}
+
+func loadDefaultEnv() {
+	v := viper.New()
+	v.SetConfigFile(".env")
+	v.AutomaticEnv()
+
+	if err := v.ReadInConfig(); err != nil {
+		log.Fatalf("Error reading configs file: %s", err)
+	}
+
+	cfg := &viperConfig{}
+	if err := v.Unmarshal(cfg); err != nil {
+		log.Fatalf("Unable to decode into struct, %v", err)
+	}
+	instance = cfg
 }
 
 func GetConfig() Config {
